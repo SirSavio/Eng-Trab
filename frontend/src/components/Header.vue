cac678c4f0bb7f46f367c858fa37c8ee4dd4a012
Erick Figueiredo <ericklima816@gmail.com>
2021-10-17 09:23:03 -0300

    UPDATED - Adição de navbar em dispositivos pequenos

--- frontend/src/components/Header.vue
+++ frontend/src/components/Header.vue
@@ -1,8 +1,7 @@
 <template>
-  <!-- <header
-    class="flex justify-between items-center py-4 px-6 bg-white border-b-4 border-green-500"
+  <header
+    class="md:hidden flex justify-between items-center py-4 px-6 bg-white border-b-4 border-green-500"
   >
-    <MessageCardFixed :type="type" :title="title" :message="message" />
     <div class="flex items-center">
       <button
         @click="changeSidebar()"
@@ -11,75 +10,13 @@
         <i class="fas fa-bars"></i>
       </button>
     </div>
-
-    <div class="flex items-center">
-      <div class="relative mx-2 lg:mx-4">
-        <button
-          @click="dropdownOpen = !dropdownOpen"
-          class="relative z-10 block h-8 w-8 rounded-full overflow-hidden shadow focus:outline-none bg-green-500 text-white transition duration-300 hover:bg-gray-500"
-        >
-          <i class="far fa-user-circle"></i>
-        </button>
-
-        <div
-          v-show="dropdownOpen"
-          @click="dropdownOpen = false"
-          class="fixed inset-0 h-full w-full z-10"
-        ></div>
-
-        <div
-          v-show="dropdownOpen"
-          class="absolute right-0 mt-2 py-2 w-48 bg-gray-100 rounded-md shadow-xl z-20"
-        >
-          <span class="block px-4 py-2 text-sm text-gray-700"
-            ><b>{{ firstName }}</b></span
-          >
-          <div>
-            <a
-              @click="log()"
-              class="block px-4 py-2 text-green-500 text-sm transition duration-300 hover:bg-green-500 hover:text-white"
-              >Sair</a
-            >
-          </div>
-        </div>
-      </div>
-    </div>
-  </header> -->
+  </header>
 </template>
-
-<style scoped>
-@media screen and (max-width: 320px) {
-  input {
-    width: 7rem;
-  }
-}
-@media screen and (min-width: 321px) and (max-width: 360px) {
-  input {
-    width: 9rem;
-  }
-}
-@media screen and (min-width: 361px) and (max-width: 450px) {
-  input {
-    width: 10rem;
-  }
-}
-@media screen and (min-width: 451px) and (max-width: 780px) {
-  input {
-    width: 16rem;
-  }
-}
-@media screen and (min-width: 781px) {
-  input {
-    width: 30rem;
-  }
-}
-</style>
 
 <script>
 
 import { mapState, mapActions, mapMutations } from "vuex";
-import MessageCardFixed from "../components/MessageCardFixed.vue";
-//import Cart from '../services/Cart';
+
 export default ({
   data() {
     return {
@@ -109,9 +46,6 @@ export default ({
       const result = await this.$store.dispatch("logout");
       this.$router.push('/')
     },
-  },
-  components: {
-    MessageCardFixed,
   },
 });
 </script>
--- frontend/src/components/Sidebar.vue
+++ frontend/src/components/Sidebar.vue
@@ -8,7 +8,7 @@
 
     <div
       :class="isOpen ? 'translate-x-0 ease-out' : '-translate-x-full ease-in'"
-      class="fixed z-30 inset-y-0 left-0 w-64 transition duration-300 transform bg-green-500 overflow-y-auto lg:translate-x-0 lg:static lg:inset-0 rounded-r-lg lg:rounded-none"
+      class="fixed z-30 inset-y-0 left-0 w-64 transition duration-300 transform bg-green-500 overflow-y-auto lg:translate-x-0 lg:static lg:inset-0 lg:rounded-none"
     >
       <div class="flex items-center justify-center mt-8">
         <router-link to="/painel">
